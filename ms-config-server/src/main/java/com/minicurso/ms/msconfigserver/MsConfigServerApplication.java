7cf397a2be57d2099aaaa3a7d2cb51687069809d
Kaynan Coelho Lira <kaynancl.kc@gmail.com>
2018-05-10 07:59:38 -0300

    create projet config server

--- ms-config-server/src/main/java/com/minicurso/ms/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/minicurso/ms/msconfigserver/MsConfigServerApplication.java
@@ -1,12 +1,16 @@
 package com.minicurso.ms.msconfigserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@SpringBootApplication
+@EnableHystrix
+@EnableConfigServer
+@SpringCloudApplication
 public class MsConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigServerApplication.class, args);
+    }
 }
