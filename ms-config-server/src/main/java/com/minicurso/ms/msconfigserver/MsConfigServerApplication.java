7931a4643f94c23c6ecc364b7870a02e62f5d076
Kaynan Coelho Lira <kaynancl.kc@gmail.com>
2018-05-10 11:56:13 -0300

    config server done

--- ms-config-server/src/main/java/com/minicurso/ms/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/minicurso/ms/msconfigserver/MsConfigServerApplication.java
@@ -1,13 +1,12 @@
 package com.minicurso.ms.msconfigserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableHystrix
+
 @EnableConfigServer
-@SpringCloudApplication
+@SpringBootApplication
 public class MsConfigServerApplication {
 
     public static void main(String[] args) {
