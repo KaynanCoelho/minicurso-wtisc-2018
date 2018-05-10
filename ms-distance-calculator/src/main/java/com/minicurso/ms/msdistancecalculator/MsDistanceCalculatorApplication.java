0f0945d2db95801b9883df2746085aad7d3c5c58
Kaynan Coelho Lira <kaynancl.kc@gmail.com>
2018-05-10 19:21:51 -0300

    integration eureka server

--- ms-distance-calculator/src/main/java/com/minicurso/ms/msdistancecalculator/MsDistanceCalculatorApplication.java
+++ ms-distance-calculator/src/main/java/com/minicurso/ms/msdistancecalculator/MsDistanceCalculatorApplication.java
@@ -2,11 +2,13 @@ package com.minicurso.ms.msdistancecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsDistanceCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsDistanceCalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsDistanceCalculatorApplication.class, args);
+    }
 }
